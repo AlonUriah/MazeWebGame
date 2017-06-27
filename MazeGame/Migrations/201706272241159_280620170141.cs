51a5f019da5aa766f7f268360c034d42a563bc00
Alon Aviv <aviv.alon@gmail.com>
2017-06-28 02:52:23 +0300

    migrations

---
+++ MazeGame/Migrations/201706272241159_280620170141.cs
@@ -0,0 +1,18 @@
+namespace MazeGame.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class _280620170141 : DbMigration
+    {
+        public override void Up()
+        {
+
+        }
+        
+        public override void Down()
+        {
+           
+        }
+    }
+}
