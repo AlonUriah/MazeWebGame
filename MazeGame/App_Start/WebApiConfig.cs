b5d25676376695104090c26e0884f94d51e6b8af
AlonUriah <alonuriah@gmail.com>
2017-06-28 17:00:05 +0300

    WebAPI Updated

--- MazeGame/App_Start/WebApiConfig.cs
+++ MazeGame/App_Start/WebApiConfig.cs
@@ -1,4 +1,7 @@
-﻿using System.Web.Http;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
 
 namespace MazeGame
 {
@@ -11,10 +14,11 @@ namespace MazeGame
             // Web API routes
             config.MapHttpAttributeRoutes();
 
+
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
+                routeTemplate: "api/{controller}/{action}/{id}",
+                defaults: new { id = RouteParameter.Optional, action = RouteParameter.Optional }
             );
         }
     }
